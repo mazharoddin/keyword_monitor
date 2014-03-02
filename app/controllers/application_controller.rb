6c74733e61cafbcb504525d3e7c1243445aefc5e
Theo Bittencourt <theodrums@gmail.com>
2014-03-02 17:04:11 -0300

    Remove Http Basic Auth on development env

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
 
-  http_basic_authenticate_with name: 'polomarte', password: 'paulomarcio'
+  unless Rails.env.development?
+    http_basic_authenticate_with name: 'polomarte', password: 'paulomarcio'
+  end
 end
