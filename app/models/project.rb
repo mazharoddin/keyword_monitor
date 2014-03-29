ac322a7d78d8d76851f0ad984b334b2fa26b7d6b
Theo Bittencourt <theodrums@gmail.com>
2014-03-29 05:20:16 -0300

    Add 'dependent: :destroy' to Project.has_many :keyword

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
-  has_many :keywords
+  has_many :keywords, dependent: :destroy
 
   validates :name, :url, presence: true
   validates :name, :url, uniqueness: true
